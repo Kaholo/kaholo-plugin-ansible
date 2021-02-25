80002755022af9e76c61a86ae2988233b76b0f2d
Amit Alon <amit@kaholo.io>
2021-02-25 21:56:25 +0200

    changes ansible only method to be runPlaybook, and added a lot of parameters for it

--- src/app.js
+++ src/app.js
@@ -1,32 +1,87 @@
-const child_process = require("child_process");
+const child_process = require(`child_process`)
 
-function executeAnsible(action) {
-    let execString = `ansible ${action.params.PARAMS}`;
-    console.log(execString)
-    return _executeSingleCommand(execString);
+async function runCLICommand(command){
+	return new Promise((resolve,reject) => {
+		child_process.exec(command, (error, stdout, stderr) => {
+			if (error) {
+			   return reject(`exec error: ${error}`);
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+			}
+			return resolve(stdout);
+		});
+	})
 }
 
-function executeAnsiblePlaybook (action) {
-    let execString = `ansible-playbook ${action.params.YAML} ${action.params.PARAMS}`;
-    return _executeSingleCommand(execString);
-}
-
-function _executeSingleCommand(command) {
-    return new Promise((resolve,reject) => {
-        child_process.exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`${stdout}`)
-                return reject(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-            }
-            return resolve(stdout);
+function getAnsibleCmd(action){
+    const playbookPath = (action.params.playbookPath || ``).trim();
+    // check if playbook path exists
+    if (!playbookPath){
+        throw `Not given Playbook Path`;
+    }
+    // create an array of args to pass in the command
+    let cmdArgs = [playbookPath];
+    // if inventories parameter was passed parse it to the cli argument format
+    const inventories = (action.params.inventories || ``).trim();
+    if (inventories){
+        inventoriess.split(`\n`).forEach(inventory => {
+            cmdArgs.push(`-i ${inventory.trim()}`);
         });
-    });
+    }
+
+    let vars = {};
+    // check if the variables parameter was passed. If so, parse it to an object
+    const paramVars = (action.params.vars || ``).trim();
+    if (paramVars){
+        try {
+            // if the variables parameter was passed as JSON it will pass
+            vars = JSON.parse(paramVars);
+        } 
+        catch { 
+            // if we catched an error then check the variables paramater was passed as key=value pairs and parse it
+            paramVars.split(`\n`).forEach(paramVar => {
+                const sep = paramVar.indexOf(`=`);
+                if (sep === -1){
+                    throw "vars were passed in a bad format";
+                }
+                vars[paramVar.substring(0, sep)] = paramVar.substring(sep + 1);
+            });
+        }
+    }
+    // check if ssh username was passed. If so also check for ssh pass, and add it to the variables object
+    const sshUser = (action.params.sshUsername || ``).trim();
+    if (sshUser){
+        const sshPass = (action.params.sshPass || ``).trim();
+        if (!sshPass){
+            throw "SSH password was expected but not provided";
+        }
+        vars.ansible_connection = "ssh";
+        vars.ansible_user = sshUser;
+        vars.ansible_ssh_pass = sshPass;
+    }
+    // check if modules paramater was passed, and if so parse it to the cli argument format
+    const modules = (action.params.modules || ``).trim();
+    if (modules){
+        cmdArgs.push(`-m ${modules.split(`\n`).map(mod => {
+            return mod.trim();
+        }).join(`,`)}`)
+    }
+    // if the variables object is not empty, parse it to the cli argument format
+    const varsArg = JSON.stringify(vars);
+    if (varsArg !== `{}`){
+        cmdArgs.push(`--extra-vars '${varsArg}'`)
+    }
+    
+    return `ansible-playbook ${cmdArgs.join(` `)}`;
 }
+
+async function runPlaybook(action){
+    const cmd = getAnsibleCmd(action);
+	return runCLICommand(cmd);
+}
+
 
 module.exports = {
-    executeAnsible : executeAnsible,
-    executeAnsiblePlaybook : executeAnsiblePlaybook
-}
+    runPlaybook
+};
