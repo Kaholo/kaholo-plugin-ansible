c81c4a8389eae780244463b687c014497cdb3ce2
Amit Alon <amit@kaholo.io>
2021-04-27 13:06:11 +0300

    added limit parameter to run playbook

--- README.md
+++ README.md
@@ -9,12 +9,10 @@ make sure your agent runs on a linux enviroment and has Ansible installed.
 Execute an Ansible playbook. 
 
 ### Parameters
-1) Playbook Path(required): The path to the Ansible playbook to execute.
-2) Inventories: The path(s) to the Ansible inventory file(s) to run the playbook on. You can enter multiple values, by seprating each
-    with a new line.
-3) SSH Username - Username for authentication with SSH.
-4) SSH Password - Password for authentication with SSH. This is a vault type so you have to enter it's value to the Kaholo vault first.
-5) Modules - The path(s) to your modules directory. You can enter multiple values, by seprating each with a new line or with ':'.
-    Default is `~/.ansible/plugins/modules:/usr/share/ansible/plugins/modules`
-6) Variables - Any extra variables to define outside of the playbook. You can enter the variables either as key=value pairs, or 
-    pass an object from code whose fields are the variables to pass. When passed as key value pair, you can enter multiple vars by sepeating each pair in a new line.
+1. Playbook Path (String) **Required** - The path to the Ansible playbook to execute.
+2. SSH Username (String) **Optional** - Username for authenticating with SSH.
+3. SSH Password (Vault) **Optional** - Password for authenticating with SSH. 
+4. Inventories (Text) **Optional** - The path(s) to the Ansible inventory file(s) to run the playbook on. Doesn't limit the playbook to run on any other playbooks specified in the yml You can enter multiple values, by seprating each with a new line.
+5. Limit (Text) **Optional** - The path(s) to the Ansible inventory file(s) to limit the playbook to run only on them. You can enter multiple values, by seprating each with a new line.
+6. Modules (Text) **Optional** - The path(s) to your modules directory. You can enter multiple values, by seprating each with a new line or with ':'.
+7. Variables (Text) **Optional** - Any extra variables to define outside of the playbook. You can enter the variables either as key=value pairs, or pass an object in the following format *{varName: value, anotherVar: val2}*. When passed as key value pair, enter multiple variables by sepeating each pair with a new line.
--- src/app.js
+++ src/app.js
@@ -1,23 +1,51 @@
-const child_process = require(`child_process`)
-const { getAnsibleCmd } = require('./helpers');
-
-async function runCLICommand(command){
-	return new Promise((resolve,reject) => {
-		child_process.exec(command, (error, stdout, stderr) => {
-			if (error) {
-			   return reject(`exec error: ${error}`);
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-			}
-			return resolve(stdout);
-		});
-	})
-}
+const { textToArgs, runCLICommand, parseVars } = require('./helpers');
 
 async function runPlaybook(action){
     const cmd = getAnsibleCmd(action);
     return runCLICommand(cmd);
+}
+
+function setSshInVars(action, vars){
+    // check if ssh username was passed. If so also check for ssh pass, and add it to the variables object
+    const sshUser = (action.params.sshUsername || ``).trim();
+    const sshPass = (action.params.sshPass || ``).trim();
+    if (sshUser){
+        if (!sshPass){
+            throw "SSH password was expected but not provided";
+        }
+        vars.ansible_connection = "ssh";
+        vars.ansible_user = sshUser;
+        vars.ansible_ssh_pass = sshPass;
+    }
+    else if (sshPass){
+        throw "SSH username was expected but not provided";
+    }
+}
+
+function getAnsibleCmd(action){
+    const playbookPath = (action.params.playbookPath || ``).trim();
+    // check if playbook path exists
+    if (!playbookPath){
+        throw `Not given Playbook Path`;
+    }
+    // create an array of args to pass in the command
+    let cmdArgs = [playbookPath];
+    // push inventories and limit parameter to args
+    textToArgs(action.params.inventories, '-i', cmdArgs);
+    textToArgs(action.params.limit, '-l', cmdArgs);
+    // parse the vars parameter to an object
+    let vars = parseVars(action.params.vars);
+    // set the ssh parameters as variables
+    setSshInVars(action, vars);
+    // push modules parameter to args
+    textToArgs(action.params.modules, "-M", cmdArgs);
+    // if the variables object is not empty, parse it to the cli argument format
+    const varsArg = JSON.stringify(vars);
+    if (varsArg !== `{}`){
+        cmdArgs.push("--extra-vars", varsArg)
+    }
+    
+    return `ansible-playbook ${cmdArgs.join(` `)}`;
 }
 
 module.exports = {
